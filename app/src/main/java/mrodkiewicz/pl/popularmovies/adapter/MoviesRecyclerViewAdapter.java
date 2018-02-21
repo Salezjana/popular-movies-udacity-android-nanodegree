66498e91c9769ec14d89721d09723445b6152462
Mikołaj Rod <mikolaj.rodkiewicz@gmail.com>
2018-02-21 23:41:20 +0100

    add Timber plant

--- app/src/main/java/mrodkiewicz/pl/popularmovies/adapter/MoviesRecyclerViewAdapter.java
+++ app/src/main/java/mrodkiewicz/pl/popularmovies/adapter/MoviesRecyclerViewAdapter.java
@@ -5,8 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import butterknife.BindView;
 import mrodkiewicz.pl.popularmovies.R;
 
 /**
@@ -18,34 +20,29 @@ public class MoviesRecyclerViewAdapter extends RecyclerView.Adapter<MoviesRecycl
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
 
-    // data is passed into the constructor
     public MoviesRecyclerViewAdapter(Context context, String[] data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
     }
 
-    // inflates the cell layout from xml when needed
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = mInflater.inflate(R.layout.list_item_movie, parent, false);
         return new ViewHolder(view);
     }
 
-    // binds the data to the textview in each cell
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         String animal = mData[position];
         holder.myTextView.setText(animal);
     }
 
-    // total number of cells
     @Override
     public int getItemCount() {
         return mData.length;
     }
 
 
-    // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView myTextView;
 
@@ -61,17 +58,15 @@ public class MoviesRecyclerViewAdapter extends RecyclerView.Adapter<MoviesRecycl
         }
     }
 
-    // convenience method for getting data at click position
     public String getItem(int id) {
         return mData[id];
     }
 
-    // allows clicks events to be caught
+
     public void setClickListener(ItemClickListener itemClickListener) {
         this.mClickListener = itemClickListener;
     }
 
-    // parent activity will implement this method to respond to click events
     public interface ItemClickListener {
         void onItemClick(View view, int position);
     }
--- app/src/main/java/mrodkiewicz/pl/popularmovies/view/MainActivity.java
+++ app/src/main/java/mrodkiewicz/pl/popularmovies/view/MainActivity.java
@@ -1,31 +1,41 @@
 package mrodkiewicz.pl.popularmovies.view;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.transition.BuildConfig;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import mrodkiewicz.pl.popularmovies.PopularMovies;
 import mrodkiewicz.pl.popularmovies.R;
 import mrodkiewicz.pl.popularmovies.adapter.MoviesRecyclerViewAdapter;
 import mrodkiewicz.pl.popularmovies.api.APIService;
-import mrodkiewicz.pl.popularmovies.helpers.Config;
 import mrodkiewicz.pl.popularmovies.model.Movie;
 import mrodkiewicz.pl.popularmovies.model.MoviesResponse;
 import mrodkiewicz.pl.popularmovies.view.base.BaseAppCompatActivity;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import timber.log.Timber;
 
 import static mrodkiewicz.pl.popularmovies.helpers.Config.API_KEY;
 
-public class MainActivity extends BaseAppCompatActivity implements MoviesRecyclerViewAdapter.ItemClickListener{
+public class MainActivity extends BaseAppCompatActivity implements MoviesRecyclerViewAdapter.ItemClickListener {
+
+    @BindView(R.id.movies_recycler_view)
+    RecyclerView moviesRecyclerView;
+
 
     private APIService service;
     private List<Movie> movies;
@@ -36,6 +46,11 @@ public class MainActivity extends BaseAppCompatActivity implements MoviesRecycle
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
 
         showProgressDialog(null, getString(R.string.download_movies));
 
@@ -46,12 +61,27 @@ public class MainActivity extends BaseAppCompatActivity implements MoviesRecycle
 
         String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
         int numberOfColumns = 6;
-        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        moviesRecyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
         moviesRecyclerViewAdapter = new MoviesRecyclerViewAdapter(this, data);
         moviesRecyclerViewAdapter.setClickListener(this);
-        recyclerView.setAdapter(moviesRecyclerViewAdapter);
+        moviesRecyclerView.setAdapter(moviesRecyclerViewAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startActivity(new Intent(this,DetailActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void loadMovies() {
@@ -62,12 +92,12 @@ public class MainActivity extends BaseAppCompatActivity implements MoviesRecycle
             Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
             call.enqueue(new Callback<MoviesResponse>() {
                 @Override
-                public void onResponse(Call<MoviesResponse>call, Response<MoviesResponse> response) {
+                public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
                     List<Movie> movies = response.body().getResults();
                 }
 
                 @Override
-                public void onFailure(Call<MoviesResponse>call, Throwable t) {
+                public void onFailure(Call<MoviesResponse> call, Throwable t) {
                 }
             });
             hideProgressDialog();
@@ -77,7 +107,7 @@ public class MainActivity extends BaseAppCompatActivity implements MoviesRecycle
                     getString(R.string.no_internet),
                     Snackbar.LENGTH_INDEFINITE)
                     .show();
-
+            hideProgressDialog();
 
         }
 
