92cddef7c874d66c85219fc5b2ec263cfecc699a
Mikołaj Rod <mikolaj.rodkiewicz@gmail.com>
2018-04-06 23:46:24 +0200

    add MoviesProvider

--- app/src/main/java/mrodkiewicz/pl/popularmovies/provider/MoviesProvider.java
+++ app/src/main/java/mrodkiewicz/pl/popularmovies/provider/MoviesProvider.java
@@ -10,52 +10,42 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import mrodkiewicz.pl.popularmovies.db.FavouritesMoviesDatebaseHelper;
+import mrodkiewicz.pl.popularmovies.helpers.Config;
 import timber.log.Timber;
 
-import static mrodkiewicz.pl.popularmovies.helpers.Config.CONTENT_AUTHORITY;
-import static mrodkiewicz.pl.popularmovies.helpers.Config.CONTENT_DIR_TYPE;
-import static mrodkiewicz.pl.popularmovies.helpers.Config.CONTENT_ITEM_TYPE;
-import static mrodkiewicz.pl.popularmovies.helpers.Config.KEY_MOVIE_ID;
-import static mrodkiewicz.pl.popularmovies.helpers.Config.TABLE_MOVIE;
-import static mrodkiewicz.pl.popularmovies.helpers.Config.buildFlavorsUri;
-
-public class MoviesProvider extends ContentProvider{
-    private FavouritesMoviesDatebaseHelper favouritesMoviesDatebaseHelper;
+public class MoviesProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private FavouritesMoviesDatebaseHelper favouritesMoviesDatebaseHelper;
 
-    private static final int FLAVOR = 100;
-    private static final int FLAVOR_WITH_ID = 200;
+    private static final int MOVIE = 100;
+    private static final int MOVIE_BY_ID = 200;
 
-    private static UriMatcher buildUriMatcher(){
+    private static UriMatcher buildUriMatcher() {
         final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
-        final String authority = CONTENT_AUTHORITY;
+        final String authority = Config.CONTENT_AUTHORITY;
 
-        matcher.addURI(authority, TABLE_MOVIE, FLAVOR);
-        matcher.addURI(authority, TABLE_MOVIE + "/#", FLAVOR_WITH_ID);
+        matcher.addURI(authority, Config.TABLE_MOVIE, MOVIE);
+        matcher.addURI(authority, Config.TABLE_MOVIE + "/#", MOVIE_BY_ID);
 
         return matcher;
     }
 
-
-
     @Override
     public boolean onCreate() {
         favouritesMoviesDatebaseHelper = new FavouritesMoviesDatebaseHelper(getContext());
-        return false;
+        return true;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
         Cursor retCursor;
-        switch(sUriMatcher.match(uri)){
-            // All Flavors selected
-            case FLAVOR:{
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE: {
                 retCursor = favouritesMoviesDatebaseHelper.getReadableDatabase().query(
-                        TABLE_MOVIE,
+                        Config.TABLE_MOVIE,
                         projection,
                         selection,
                         selectionArgs,
@@ -64,20 +54,18 @@ public class MoviesProvider extends ContentProvider{
                         sortOrder);
                 return retCursor;
             }
-            // Individual flavor based on Id selected
-            case FLAVOR_WITH_ID:{
+            case MOVIE_BY_ID: {
                 retCursor = favouritesMoviesDatebaseHelper.getReadableDatabase().query(
-                        TABLE_MOVIE,
+                        Config.TABLE_MOVIE,
                         projection,
-                        KEY_MOVIE_ID + " = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))},
+                        Config.MovieEntry._ID + " = ?",
+                        new String[]{String.valueOf(ContentUris.parseId(uri))},
                         null,
                         null,
                         sortOrder);
                 return retCursor;
             }
-            default:{
-                // By default, we assume a bad URI
+            default: {
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
             }
         }
@@ -88,14 +76,14 @@ public class MoviesProvider extends ContentProvider{
     public String getType(@NonNull Uri uri) {
         final int match = sUriMatcher.match(uri);
 
-        switch (match){
-            case FLAVOR:{
-                return CONTENT_DIR_TYPE;
+        switch (match) {
+            case MOVIE: {
+                return Config.MovieEntry.CONTENT_DIR_TYPE;
             }
-            case FLAVOR_WITH_ID:{
-                return CONTENT_ITEM_TYPE;
+            case MOVIE_BY_ID: {
+                return Config.MovieEntry.CONTENT_ITEM_TYPE;
             }
-            default:{
+            default: {
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
             }
         }
@@ -107,11 +95,10 @@ public class MoviesProvider extends ContentProvider{
         final SQLiteDatabase db = favouritesMoviesDatebaseHelper.getWritableDatabase();
         Uri returnUri;
         switch (sUriMatcher.match(uri)) {
-            case FLAVOR: {
-                long _id = db.insert(TABLE_MOVIE, null, values);
-                // insert unless it is already contained in the database
+            case MOVIE: {
+                long _id = db.insert(Config.TABLE_MOVIE, null, values);
                 if (_id > 0) {
-                    returnUri = buildFlavorsUri(_id);
+                    returnUri = Config.MovieEntry.buildFlavorsUri(_id);
                 } else {
                     throw new android.database.SQLException("Failed to insert row into: " + uri);
                 }
@@ -132,21 +119,19 @@ public class MoviesProvider extends ContentProvider{
         final SQLiteDatabase db = favouritesMoviesDatebaseHelper.getWritableDatabase();
         final int match = sUriMatcher.match(uri);
         int numDeleted;
-        switch(match){
-            case FLAVOR:
+        switch (match) {
+            case MOVIE:
                 numDeleted = db.delete(
-                        TABLE_MOVIE, selection, selectionArgs);
-                // reset _ID
+                        Config.TABLE_MOVIE, selection, selectionArgs);
                 db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" +
-                        TABLE_MOVIE + "'");
+                        Config.TABLE_MOVIE + "'");
                 break;
-            case FLAVOR_WITH_ID:
-                numDeleted = db.delete(TABLE_MOVIE,
-                        KEY_MOVIE_ID + " = ?",
+            case MOVIE_BY_ID:
+                numDeleted = db.delete(Config.TABLE_MOVIE,
+                        Config.MovieEntry._ID + " = ?",
                         new String[]{String.valueOf(ContentUris.parseId(uri))});
-                // reset _ID
                 db.execSQL("DELETE FROM SQLITE_SEQUENCE WHERE NAME = '" +
-                        TABLE_MOVIE + "'");
+                        Config.TABLE_MOVIE + "'");
 
                 break;
             default:
@@ -157,44 +142,36 @@ public class MoviesProvider extends ContentProvider{
     }
 
     @Override
-    public int bulkInsert(Uri uri, ContentValues[] values){
+    public int bulkInsert(Uri uri, ContentValues[] values) {
         final SQLiteDatabase db = favouritesMoviesDatebaseHelper.getWritableDatabase();
         final int match = sUriMatcher.match(uri);
-        switch(match){
-            case FLAVOR:
-                // allows for multiple transactions
+        switch (match) {
+            case MOVIE:
                 db.beginTransaction();
-
-                // keep track of successful inserts
                 int numInserted = 0;
-                try{
-                    for(ContentValues value : values){
-                        if (value == null){
+                try {
+                    for (ContentValues value : values) {
+                        if (value == null) {
                             throw new IllegalArgumentException("Cannot have null content values");
                         }
                         long _id = -1;
-                        try{
-                            _id = db.insertOrThrow(TABLE_MOVIE,
+                        try {
+                            _id = db.insertOrThrow(Config.TABLE_MOVIE,
                                     null, value);
-                        }catch(SQLiteConstraintException e) {
-                            Timber.d("Attempting to insert  but value is already in database.");
-                    }
-                        if (_id != -1){
+                        } catch (SQLiteConstraintException e) {
+                            Timber.d("SQLiteConstraintException " + e);
+                        }
+                        if (_id != -1) {
                             numInserted++;
                         }
                     }
-                    if(numInserted > 0){
-                        // If no errors, declare a successful transaction.
-                        // database will not populate if this is not called
+                    if (numInserted > 0) {
                         db.setTransactionSuccessful();
                     }
                 } finally {
-                    // all transactions occur at once
                     db.endTransaction();
                 }
-                if (numInserted > 0){
-                    // if there was successful insertion, notify the content resolver that there
-                    // was a change
+                if (numInserted > 0) {
                     getContext().getContentResolver().notifyChange(uri, null);
                 }
                 return numInserted;
@@ -204,39 +181,39 @@ public class MoviesProvider extends ContentProvider{
     }
 
     @Override
-    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs){
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
         final SQLiteDatabase db = favouritesMoviesDatebaseHelper.getWritableDatabase();
         int numUpdated = 0;
 
-        if (contentValues == null){
+        if (values == null) {
             throw new IllegalArgumentException("Cannot have null content values");
         }
 
-        switch(sUriMatcher.match(uri)){
-            case FLAVOR:{
-                numUpdated = db.update(TABLE_MOVIE,
-                        contentValues,
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE: {
+                numUpdated = db.update(Config.TABLE_MOVIE,
+                        values,
                         selection,
                         selectionArgs);
                 break;
             }
-            case FLAVOR_WITH_ID: {
-                numUpdated = db.update(TABLE_MOVIE,
-                        contentValues,
-                        KEY_MOVIE_ID + " = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))});
+            case MOVIE_BY_ID: {
+                numUpdated = db.update(Config.TABLE_MOVIE,
+                        values,
+                        Config.MovieEntry._ID + " = ?",
+                        new String[]{String.valueOf(ContentUris.parseId(uri))});
                 break;
             }
-            default:{
+            default: {
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
             }
         }
 
-        if (numUpdated > 0){
+        if (numUpdated > 0) {
             getContext().getContentResolver().notifyChange(uri, null);
         }
 
         return numUpdated;
     }
-
 }
+
